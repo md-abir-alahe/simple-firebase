c192cf8227ef839dec22145e56d21721fb2639de
md-abir-alahe <mdabiralahe@gmail.com>
2023-04-16 15:15:39 +0600

    display logged in user info, sign out, toggle sign in, sign out

--- src/_steps.js
+++ src/_steps.js
@@ -12,4 +12,5 @@
 11. import google auth provider and create a new provider
 12. use signInWithPopUp and pass auth and provider
 13. activate sign-in method (google,facebook)
+14. [vite]: change 127.0.0.1 to localhost
 */
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,29 +1,58 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import app from '../../firebase/firebase.inti';
+import React, { useState } from "react";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "../../firebase/firebase.inti";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
 
-    const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
 
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
-        .then(result => {
-            const user = result.user;
-            console.log(user);
-        })
-        .catch(error=>{
-            console.log(error);
-        })
-    }
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    return (
+  return (
+    <div>
+      {/* user ? logout : sign in */}
+      {user ? (
+        <button onClick={handleSignOut}>Sign out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google Log in</button>
+      )}
+      {user && (
         <div>
-            <button onClick={handleGoogleSignIn}>Google Log in</button>
+          <h3>User : {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Login;
